80f3619fe27a35c5e5867a18bc5d8375960d49a9
ccmanlapus <152623112+ccmanlapus@users.noreply.github.com>
2024-04-21 03:41:30 +0800

    Partial Created Students View
    Nag buhat ko ug Student view ug nag edit ko sa card para maka link ko sa student view and nag buhat sad ko ug StudendTemplate para ma remove ang side view and admin view

---
+++ web/src/pages/studentStatusDashboard/index.page.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+// import { FaRegChartBar } from 'react-icons/fa'
+import CardItem from '@/components/organisms/Card'
+import PageHeader from '@/components/organisms/PageHeader'
+import StudentTemplate from '@/components/templates/StudentTemplate'
+
+const studentStatusDashboard = () => {
+  const breadcrumbs = [
+    {
+      href: '#',
+      title: 'SICC Online Admission Application Portal',
+    //   icon: FaRegChartBar,
+    },
+  ]
+
+  const cardData = [
+    { title: 'Online Application Form', description: 'Visit this site to reserve your admission test here.', link: '/Registration' } ,
+    { title: 'Check Your Enrollment Status', description: 'Visit this site to check your admission slot here.', link: 'https://example.com/check-enrollment-status' },
+    // { title: '1000', description: 'BS Agri-Business' },
+  ]
+
+  return (
+    <StudentTemplate>
+      <PageHeader breadcrumbs={breadcrumbs} />
+
+      <div className='m-5 grid gap-5 mb-6 md:grid-cols-3'>
+        {cardData.map((card, index) => (
+          <CardItem
+            key={index}
+            title={card.title}
+            description={card.description}
+            link={card.link}
+          />
+        ))}
+      </div>
+    </StudentTemplate>
+  )
+}
+
+export default studentStatusDashboard
