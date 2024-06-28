0cba207260e89f6e8a01dabedfce84f672f0edbc
Kiel <89174822+Kiel-1401@users.noreply.github.com>
2024-06-29 00:55:06 +0800

    gi commit ra nako

--- web/src/pages/Registration/[registrationId]/index.page.jsx
+++ web/src/pages/Registration/[registrationId]/index.page.jsx
@@ -1,22 +1,22 @@
-import dayjs from 'dayjs';
-import { Button } from 'flowbite-react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { FaCalendarAlt, FaMapMarkerAlt, FaUser } from 'react-icons/fa';
+import dayjs from 'dayjs'
+import { Button } from 'flowbite-react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { FaCalendarAlt, FaMapMarkerAlt, FaUser } from 'react-icons/fa'
 
-import Loading from '@/components/atoms/Loading';
-import DeleteModal from '@/components/organisms/DeleteModal';
-import PageHeader from '@/components/organisms/PageHeader';
-import RowItem from '@/components/organisms/RowItem';
-import Template from '@/components/templates/Template';
+import Loading from '@/components/atoms/Loading'
+// import DeleteModal from '@/components/organisms/DeleteModal'
+import PageHeader from '@/components/organisms/PageHeader'
+import RowItem from '@/components/organisms/RowItem'
+import Template from '@/components/templates/Template'
 
-import useHooks from './hooks';
+import useHooks from './hooks'
 
 const Registration = () => {
-  const router = useRouter();
-  const { registrationId } = router.query;
+  const router = useRouter()
+  const { registrationId } = router.query
 
-  const { registration, isLoading, handleDelete } = useHooks(registrationId);
+  const { registration, isLoading } = useHooks(registrationId)
 
   const breadcrumbs = [
     {
@@ -28,7 +28,7 @@ const Registration = () => {
       href: '#',
       title: 'Registration Detail',
     },
-  ];
+  ]
 
   return (
     <Template>
@@ -36,7 +36,7 @@ const Registration = () => {
         breadcrumbs={breadcrumbs}
         right={
           <div className='flex pb-4 space-x-4'>
-            <DeleteModal handleDelete={() => handleDelete(registrationId)} />
+            {/* <DeleteModal handleDelete={() => handleDelete(registrationId)} /> */}
             <Link href={`/registration/${registrationId}/edit`}>
               <Button size='xs' color='warning' className='m-w-20'>
                 Edit
@@ -45,10 +45,9 @@ const Registration = () => {
           </div>
         }
       />
-      {isLoading || !registration ? (
+      {isLoading || !registration ?
         <Loading />
-      ) : (
-        <section className='p-8 flex flex-col space-x-4 space-y-6'>
+      : <section className='p-8 flex flex-col space-x-4 space-y-6'>
           <div className='flex space-x-8'>
             <RowItem
               label='Date'
@@ -66,7 +65,10 @@ const Registration = () => {
           <RowItem label='Middle Name' value={registration.mname} />
           <RowItem label='Suffix' value={registration.pref} />
           <RowItem label='Age' value={registration.age} />
-          <RowItem label='Birthdate' value={`${registration.monthoption} ${registration.date}, ${registration.year}`} />
+          <RowItem
+            label='Birthdate'
+            value={`${registration.monthoption} ${registration.date}, ${registration.year}`}
+          />
           <RowItem label='Sex' value={registration.sex} />
           <RowItem label='Gender' value={registration.gender} />
           <RowItem label='Civil Status' value={registration.civilstatus} />
@@ -74,7 +76,9 @@ const Registration = () => {
           <RowItem label='Email' value={registration.email} />
           <RowItem label='Place of Birth' value={registration.pbirth} />
           <RowItem label='Indigent' value={registration.indigentP} />
-          {registration.indigentPy && <RowItem label='Indigenous Group' value={registration.indigentPy} />}
+          {registration.indigentPy && (
+            <RowItem label='Indigenous Group' value={registration.indigentPy} />
+          )}
           <RowItem label='Purok/Block/Sitio' value={registration.pbs} />
           <RowItem label='District' value={registration.district} />
           <RowItem label='Barangay' value={registration.barangay} />
@@ -82,18 +86,30 @@ const Registration = () => {
           <RowItem label='Province' value={registration.province} />
           <RowItem label='Zip Code' value={registration.Zcode} />
           <RowItem label='Family Background' value={registration.familyB} />
-          {registration.sincewhen && <RowItem label='Since When' value={registration.sincewhen} />}
+          {registration.sincewhen && (
+            <RowItem label='Since When' value={registration.sincewhen} />
+          )}
           <RowItem label='Number of Siblings' value={registration.Nsibling} />
           <RowItem label='Support for Study' value={registration.supstudy} />
           <RowItem label='OFW' value={registration.ofw} />
-          {registration.ofwProfession && <RowItem label='OFW Profession' value={registration.ofwProfession} />}
+          {registration.ofwProfession && (
+            <RowItem
+              label='OFW Profession'
+              value={registration.ofwProfession}
+            />
+          )}
           <RowItem label='Student Category' value={registration.StudentCat} />
-          {registration.Nwork && <RowItem label='Nature of Work' value={registration.Nwork} />}
+          {registration.Nwork && (
+            <RowItem label='Nature of Work' value={registration.Nwork} />
+          )}
           <RowItem label='Student Type' value={registration.studenttype} />
           {registration.F_nameSchool && (
             <div>
               <p>Freshmen:</p>
-              <RowItem label='Last School Attended' value={registration.F_nameSchool} />
+              <RowItem
+                label='Last School Attended'
+                value={registration.F_nameSchool}
+              />
               <RowItem label='Academic Track' value={registration.F_Atrack} />
               <RowItem label='Address' value={registration.F_AMprovince} />
               <RowItem label='Year Graduated' value={registration.F_Ygrad} />
@@ -102,7 +118,10 @@ const Registration = () => {
           {registration.T_nameSchool && (
             <div>
               <p>Transferee:</p>
-              <RowItem label='Last School Attended' value={registration.T_nameSchool} />
+              <RowItem
+                label='Last School Attended'
+                value={registration.T_nameSchool}
+              />
               <RowItem label='Course' value={registration.T_Atrack} />
               <RowItem label='Address' value={registration.T_AMprovince} />
               <RowItem label='Year Attended' value={registration.T_Ygrad} />
@@ -110,9 +129,9 @@ const Registration = () => {
           )}
           <RowItem label='Course' value={registration.selectcourse} />
         </section>
-      )}
+      }
     </Template>
-  );
-};
+  )
+}
 
-export default Registration;
+export default Registration
