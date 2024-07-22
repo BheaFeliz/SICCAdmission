b7c137d846cfd8933c17694d96cbe36a0371d918
bhea <bhea.gante@gmail.com>
2024-07-22 12:00:58 +0800

    Update

--- web/src/pages/referenceInput/index.page.jsx
+++ web/src/pages/referenceInput/index.page.jsx
@@ -40,7 +40,7 @@ function Component() {
                 href={`/referenceRev?ref=${referenceNumber}`}
                 className='mt-12 inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700'
               >
-                Search
+                Submit
               </Link>
             </div>
           </div>
--- web/src/pages/referenceRev/index.page.jsx
+++ web/src/pages/referenceRev/index.page.jsx
@@ -1,15 +1,31 @@
 import { Card } from 'flowbite-react'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import PageHeader from '@/components/organisms/PageHeader'
 import Template from '@/components/templates/StudentTemplate'
+import { useGetScheduleByIdQuery } from '@/hooks/api/scheduleApi'
 import { useGetRegistrationsQuery } from '@/hooks/api/studentApi'
+import { Scourse } from '@/hooks/redux/const'
 
-function InfoTable({ name, course, classroom, schoolYear, referenceNumber }) {
+function InfoTable({
+  name,
+  course,
+  referenceNumber,
+  scheduleDate,
+  scheduleStartTime,
+  scheduleEndTime,
+  title,
+  description,
+}) {
   return (
     <table className='w-full'>
+      <thead className='bg-gray-200'>
+        <tr>
+          <th className='px-4 py-2 font-semibold'>Reference Number:</th>
+          <th className='px-4 py-2'>{referenceNumber}</th>
+        </tr>
+      </thead>
       <tbody className='bg-white divide-y divide-gray-200'>
         <tr>
           <td className='px-4 py-2 font-semibold'>Name:</td>
@@ -20,16 +36,24 @@ function InfoTable({ name, course, classroom, schoolYear, referenceNumber }) {
           <td className='px-4 py-2'>{course}</td>
         </tr>
         <tr>
-          <td className='px-4 py-2 font-semibold'>Room Name:</td>
-          <td className='px-4 py-2'>{classroom}</td>
+          <td className='px-4 py-2 font-semibold'>Scheduled Date:</td>
+          <td className='px-4 py-2'>{scheduleDate}</td>
         </tr>
         <tr>
-          <td className='px-4 py-2 font-semibold'>School Year:</td>
-          <td className='px-4 py-2'>{schoolYear}</td>
+          <td className='px-4 py-2 font-semibold'>Scheduled Start Time:</td>
+          <td className='px-4 py-2'>{scheduleStartTime}</td>
         </tr>
         <tr>
-          <td className='px-4 py-2 font-semibold'>Reference Number:</td>
-          <td className='px-4 py-2'>{referenceNumber}</td>
+          <td className='px-4 py-2 font-semibold'>Scheduled End Time:</td>
+          <td className='px-4 py-2'>{scheduleEndTime}</td>
+        </tr>
+        <tr>
+          <td className='px-4 py-2 font-semibold'>Title:</td>
+          <td className='px-4 py-2'>{title}</td>
+        </tr>
+        <tr>
+          <td className='px-4 py-2 font-semibold'>Description:</td>
+          <td className='px-4 py-2'>{description}</td>
         </tr>
       </tbody>
     </table>
@@ -39,39 +63,83 @@ function InfoTable({ name, course, classroom, schoolYear, referenceNumber }) {
 function Component() {
   const router = useRouter()
   const { ref } = router.query
-  const { data, error, isLoading } = useGetRegistrationsQuery()
+  const {
+    data: registrationsData,
+    error: registrationsError,
+    isLoading: isLoadingRegistrations,
+  } = useGetRegistrationsQuery()
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Error loading data</div>
+  const [scheduleId, setScheduleId] = useState(null)
 
-  // Filter the registration by reference number
-  const registration = data?.registrations.find(
+  useEffect(() => {
+    if (registrationsData) {
+      const registration = registrationsData.registrations.find(
+        (reg) => reg.reference_number === ref,
+      )
+      if (registration) {
+        setScheduleId(registration.schedule_id)
+      }
+    }
+  }, [registrationsData, ref])
+
+  const {
+    data: scheduleData,
+    error: scheduleError,
+    isLoading: isLoadingSchedule,
+  } = useGetScheduleByIdQuery(scheduleId, {
+    skip: !scheduleId,
+  })
+
+  useEffect(() => {
+    if (scheduleData) {
+      console.log('Schedule Data:', scheduleData)
+    }
+  }, [scheduleData])
+
+  if (isLoadingRegistrations || isLoadingSchedule) return <div>Loading...</div>
+  if (registrationsError || scheduleError) return <div>Error loading data</div>
+
+  if (!registrationsData || !scheduleData)
+    return <div>No registration found</div>
+
+  const registration = registrationsData.registrations.find(
     (reg) => reg.reference_number === ref,
   )
 
-  if (!registration) return <div>No registration found</div>
+  const courseLabelMap = Scourse.reduce((acc, course) => {
+    acc[course.value] = course.label
+    return acc
+  }, {})
 
   return (
     <Template>
       <PageHeader>Admission Application Status</PageHeader>
 
+      <div className='m-2'>
+        <Card>
+          <p>
+            Fill out this form carefully and TYPE all the information requested.
+            Select the appropriate choices. if the item is not applicable
+            indicate select or type N/A. INCOMPLETE FORMS WILL BE NOT PROCESSED.
+          </p>
+        </Card>
+      </div>
+
       <div className='container mx-auto mt-8'>
         <Card className='w-full mb-20'>
           <InfoTable
             name={`${registration.fname} ${registration.lname}`}
-            course={registration.selectcourse}
-            classroom={registration.classroom}
-            schoolYear={registration.schoolYear}
+            course={
+              courseLabelMap[registration.selectcourse] ||
+              registration.selectcourse
+            }
+            scheduleDate={scheduleData ? scheduleData.date : 'N/A'}
+            scheduleStartTime={scheduleData ? scheduleData.startTime : 'N/A'}
+            scheduleEndTime={scheduleData ? scheduleData.endTime : 'N/A'}
             referenceNumber={registration.reference_number}
+            title={scheduleData ? scheduleData.title : 'N/A'}
+            description={scheduleData ? scheduleData.description : 'N/A'}
           />
-          <div className='w-full px-1 mt-2 flex justify-end'>
-            <Link
-              href='/studentStatusDashboard'
-              className='inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700'
-            >
-              Done
-            </Link>
-          </div>
         </Card>
       </div>
     </Template>
