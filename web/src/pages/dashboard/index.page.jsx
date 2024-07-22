f045686e76c7b89ed536ed205b0d48934edfa61f
ccmanlapus <152623112+ccmanlapus@users.noreply.github.com>
2024-07-22 14:29:15 +0800

    fix some bug

--- web/src/pages/dashboard/index.page.jsx
+++ web/src/pages/dashboard/index.page.jsx
@@ -18,7 +18,6 @@ const Dashboard = () => {
     },
   ];
 
-  // Create a mapping from course codes to labels
   const courseLabelMap = Scourse.reduce((acc, course) => {
     acc[course.value] = course.label;
     return acc;
@@ -32,7 +31,6 @@ const Dashboard = () => {
   return (
     <Template>
       <PageHeader breadcrumbs={breadcrumbs} />
-
       <div className='mx-auto max-w-screen-lg mt-12'>
         {isLoading ? (
           <div className="flex justify-center items-center h-64">
