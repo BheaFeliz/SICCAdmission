97217a9c8178846aa629de3a94386a03e6b997ef
Kiel <89174822+Kiel-1401@users.noreply.github.com>
2024-07-25 01:27:37 +0800

    corrected dashboard with stafftemplate

--- web/src/components/organisms/Navbar.jsx
+++ web/src/components/organisms/Navbar.jsx
@@ -2,15 +2,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { HiLogout, HiOutlineUserCircle } from 'react-icons/hi'
 
+import { useUser } from '@/hooks/redux/auth'
+
 const Navbar = () => {
+  const { user } = useUser()
   return (
     <nav className='p-3 px-8 flex justify-between items-center bg-blue-100'>
       <Link href='/' className='font-bold flex items-center'>
         <Image src='/logo.png' width={50} height={50} alt='logo' />
-        <span className='px-2 text-xl font-medium'>Samal Island City College</span>
+        <span className='px-2 text-xl font-medium'>
+          Samal Island City College
+        </span>
       </Link>
 
       <div className='flex gap-5 '>
+        {user.name}
         <Link href='/profile'>
           <HiOutlineUserCircle className='cursor-pointer hover:text-orange-700 text-xl' />
         </Link>
