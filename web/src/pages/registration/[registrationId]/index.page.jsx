5e383d60e6321722e2ce5ec07ff2c2945a317d7b
Kiel <89174822+Kiel-1401@users.noreply.github.com>
2024-08-28 11:25:33 +0800

    staff template in edit students for staffs

--- web/src/pages/registration/[registrationId]/index.page.jsx
+++ web/src/pages/registration/[registrationId]/index.page.jsx
@@ -1,14 +1,16 @@
 import dayjs from 'dayjs'
 import { Button, Card } from 'flowbite-react'
-import Link from 'next/link' // Import Link from next/link
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FaUser } from 'react-icons/fa'
 
 import Loading from '@/components/atoms/Loading'
-// import DeleteModal from '@/components/organisms/DeleteModal'
+// import DeleteModal from '@/components/organisms/DeleteModal';
 import PageHeader from '@/components/organisms/PageHeader'
 import RowItem from '@/components/organisms/RowItem'
+import StaffTemplate from '@/components/templates/StaffTemplate'
 import Template from '@/components/templates/Template'
+import { useUser } from '@/hooks/redux/auth'
 import {
   Civilstatus,
   famBackground,
@@ -27,10 +29,12 @@ import useHooks from './hooks'
 const Registration = () => {
   const router = useRouter()
   const { registrationId } = router.query
+  const { user } = useUser()
 
   const { registration, isLoading } = useHooks(registrationId)
 
   const { courses } = useCourses()
+
   const breadcrumbs = [
     {
       href: '/students',
@@ -93,129 +97,141 @@ const Registration = () => {
     return map
   }, {})
 
+  const Content = () => (
+    <section className='p-8 flex flex-col space-x-4 space-y-6'>
+      <div className='flex space-x-8'>
+        <RowItem
+          label='Date'
+          value={dayjs(registration.created_at).format('MMM DD, YYYY')}
+        />
+      </div>
+      <Card>
+        <RowItem label='First Name' value={registration.fname} />
+        <RowItem label='Last Name' value={registration.lname} />
+        <RowItem label='Middle Name' value={registration.mname} />
+        <RowItem label='Suffix' value={registration.pref} />
+        <RowItem label='Age' value={registration.age} />
+        <RowItem
+          label='Birthdate'
+          value={`${MonthoptionLabelMap[registration.monthoption]} ${registration.date}, ${registration.year}`}
+        />
+        <RowItem label='Sex' value={sexLabelMap[registration.sex]} />
+        <RowItem label='Gender' value={GenderLabelMap[registration.gender]} />
+        <RowItem
+          label='Civil Status'
+          value={CivilstatusLabelMap[registration.civilstatus]}
+        />
+        <RowItem label='Contact Number' value={registration.contactnumber} />
+        <RowItem label='Email' value={registration.email} />
+        <RowItem label='Place of Birth' value={registration.pbirth} />
+        <RowItem
+          label='Indigent'
+          value={IndigentPLabelMap[registration.indigentP]}
+        />
+        {registration.indigentPy && (
+          <RowItem label='Indigenous Group' value={registration.indigentPy} />
+        )}
+        <RowItem label='Purok/Block/Sitio' value={registration.pbs} />
+        <RowItem
+          label='District'
+          value={districtLabelMap[registration.district]}
+        />
+        <RowItem label='Barangay' value={registration.barangay} />
+        <RowItem label='City/Municipality' value={registration.cityM} />
+        <RowItem label='Province' value={registration.province} />
+        <RowItem label='Zip Code' value={registration.Zcode} />
+        <RowItem
+          label='Family Background'
+          value={FamBackgroundLabelMap[registration.familyB]}
+        />
+        {registration.sincewhen && (
+          <RowItem label='Since When' value={registration.sincewhen} />
+        )}
+        <RowItem label='Number of Siblings' value={registration.Nsibling} />
+        <RowItem label='Support for Study' value={registration.supstudy} />
+        <RowItem label='OFW' value={registration.ofw} />
+        {registration.ofwProfession && (
+          <RowItem label='OFW Profession' value={registration.ofwProfession} />
+        )}
+        <RowItem
+          label='Student Category'
+          value={ScategoryLabelMap[registration.StudentCat]}
+        />
+        {registration.Nwork && (
+          <RowItem label='Nature of Work' value={registration.Nwork} />
+        )}
+        <RowItem
+          label='Student Type'
+          value={StudenttypeLabelMap[registration.studenttype]}
+        />
+        {registration.F_nameSchool && (
+          <div>
+            <p>Freshmen:</p>
+            <RowItem
+              label='Last School Attended'
+              value={registration.F_nameSchool}
+            />
+            <RowItem label='Academic Track' value={registration.F_Atrack} />
+            <RowItem label='Address' value={registration.F_AMprovince} />
+            <RowItem label='Year Graduated' value={registration.F_Ygrad} />
+          </div>
+        )}
+        {registration.T_nameSchool && (
+          <div>
+            <p>Transferee:</p>
+            <RowItem
+              label='Last School Attended'
+              value={registration.T_nameSchool}
+            />
+            <RowItem label='Course' value={registration.T_Atrack} />
+            <RowItem label='Address' value={registration.T_AMprovince} />
+            <RowItem label='Year Attended' value={registration.T_Ygrad} />
+          </div>
+        )}
+        <RowItem label='Course' value={courseMap[registration.courseId]} />
+      </Card>
+    </section>
+  )
+
   return (
-    <Template>
-      <PageHeader
-        breadcrumbs={breadcrumbs}
-        right={
-          <div className='flex pb-4 space-x-4'>
-            <Link href={`/registration/${registrationId}/edit`}>
-              <Button size='lg' color='warning' className='m-w-20'>
-                Edit
-              </Button>
-            </Link>
-          </div>
-        }
-      />
-      {isLoading || !registration ?
-        <Loading />
-      : <section className='p-8 flex flex-col space-x-4 space-y-6'>
-          <div className='flex space-x-8'>
-            <RowItem
-              label='Date'
-              value={dayjs(registration.created_at).format('MMM DD, YYYY')}
-            />
-          </div>
-          <Card>
-            <RowItem label='First Name' value={registration.fname} />
-            <RowItem label='Last Name' value={registration.lname} />
-            <RowItem label='Middle Name' value={registration.mname} />
-            <RowItem label='Suffix' value={registration.pref} />
-            <RowItem label='Age' value={registration.age} />
-            <RowItem
-              label='Birthdate'
-              value={`${MonthoptionLabelMap[registration.monthoption]} ${registration.date}, ${registration.year}`}
-            />
-            <RowItem label='Sex' value={sexLabelMap[registration.sex]} />
-            <RowItem
-              label='Gender'
-              value={GenderLabelMap[registration.gender]}
-            />
-            <RowItem
-              label='Civil Status'
-              value={CivilstatusLabelMap[registration.civilstatus]}
-            />
-            <RowItem
-              label='Contact Number'
-              value={registration.contactnumber}
-            />
-            <RowItem label='Email' value={registration.email} />
-            <RowItem label='Place of Birth' value={registration.pbirth} />
-            <RowItem
-              label='Indigent'
-              value={IndigentPLabelMap[registration.indigentP]}
-            />
-            {registration.indigentPy && (
-              <RowItem
-                label='Indigenous Group'
-                value={registration.indigentPy}
-              />
-            )}
-            <RowItem label='Purok/Block/Sitio' value={registration.pbs} />
-            <RowItem
-              label='District'
-              value={districtLabelMap[registration.district]}
-            />
-            <RowItem label='Barangay' value={registration.barangay} />
-            <RowItem label='City/Municipality' value={registration.cityM} />
-            <RowItem label='Province' value={registration.province} />
-            <RowItem label='Zip Code' value={registration.Zcode} />
-            <RowItem
-              label='Family Background'
-              value={FamBackgroundLabelMap[registration.familyB]}
-            />
-            {registration.sincewhen && (
-              <RowItem label='Since When' value={registration.sincewhen} />
-            )}
-            <RowItem label='Number of Siblings' value={registration.Nsibling} />
-            <RowItem label='Support for Study' value={registration.supstudy} />
-            <RowItem label='OFW' value={registration.ofw} />
-            {registration.ofwProfession && (
-              <RowItem
-                label='OFW Profession'
-                value={registration.ofwProfession}
-              />
-            )}
-            <RowItem
-              label='Student Category'
-              value={ScategoryLabelMap[registration.StudentCat]}
-            />
-            {registration.Nwork && (
-              <RowItem label='Nature of Work' value={registration.Nwork} />
-            )}
-            <RowItem
-              label='Student Type'
-              value={StudenttypeLabelMap[registration.studenttype]}
-            />
-            {registration.F_nameSchool && (
-              <div>
-                <p>Freshmen:</p>
-                <RowItem
-                  label='Last School Attended'
-                  value={registration.F_nameSchool}
-                />
-                <RowItem label='Academic Track' value={registration.F_Atrack} />
-                <RowItem label='Address' value={registration.F_AMprovince} />
-                <RowItem label='Year Graduated' value={registration.F_Ygrad} />
+    <>
+      {user?.role === 'admin' ?
+        <Template>
+          <PageHeader
+            breadcrumbs={breadcrumbs}
+            right={
+              <div className='flex pb-4 space-x-4'>
+                <Link href={`/registration/${registrationId}/edit`}>
+                  <Button size='lg' color='warning' className='m-w-20'>
+                    Edit
+                  </Button>
+                </Link>
               </div>
-            )}
-            {registration.T_nameSchool && (
-              <div>
-                <p>Transferee:</p>
-                <RowItem
-                  label='Last School Attended'
-                  value={registration.T_nameSchool}
-                />
-                <RowItem label='Course' value={registration.T_Atrack} />
-                <RowItem label='Address' value={registration.T_AMprovince} />
-                <RowItem label='Year Attended' value={registration.T_Ygrad} />
+            }
+          />
+          {isLoading || !registration ?
+            <Loading />
+          : <Content />}
+        </Template>
+      : <StaffTemplate>
+          <PageHeader
+            breadcrumbs={breadcrumbs}
+            right={
+              <div className='flex pb-4 space-x-4'>
+                <Link href={`/registration/${registrationId}/edit`}>
+                  <Button size='lg' color='warning' className='m-w-20'>
+                    Edit
+                  </Button>
+                </Link>
               </div>
-            )}
-            <RowItem label='Course' value={courseMap[registration.courseId]} />
-          </Card>
-        </section>
+            }
+          />
+          {isLoading || !registration ?
+            <Loading />
+          : <Content />}
+        </StaffTemplate>
       }
-    </Template>
+    </>
   )
 }
 
